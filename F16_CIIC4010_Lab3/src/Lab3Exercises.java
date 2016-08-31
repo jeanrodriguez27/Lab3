9a2df9472981952f6a59908401483f95d1fcb495
4015a17 <4015a17@aguacate.ece.uprm.edu>
2016-08-31 08:42:16 -0400

    The following changes were made: -The frame's title is now "Jean Carlos " -The frame's size was changed to 200x200 -The frame will now appear on or close to the center of the screen when the screen's resolution is 1920x1080. -Uncommented line 6 (setDefaultCloseOperation)

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jean Carlos");
+		JFrame myFrame = new JFrame("Jean Carlos ");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(860, 440);
 		myFrame.setSize(200, 200);
